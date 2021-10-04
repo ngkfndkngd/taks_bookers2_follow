63fe86847434ad96c6e660548056bb21cd7911c1
ngkfndkngd <huku.0033@gmail.com>
2021-10-04 07:42:53 +0000

    [Finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,23 @@ class User < ApplicationRecord
   has_many :books
 	has_many :favorites, dependent: :destroy
 	has_many :book_comments, dependent: :destroy
+	
+	# 自分がフォローされる側の中間テーブルへのアソシエーション
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローする側の中間テーブルへのアソシエーション
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォローされる側からフォローしているユーザを取得する
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # フォローする側からフォローされたユーザを取得する
+  has_many :followings, through: :relationships, source: :followed
+  
   attachment :profile_image, destroy: false
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  
+  # あるユーザが引数で渡されたuserにフォローされているか調べるメソッド
+  def is_followed_by?(user)
+    reverse_of_relationships.find_by(follower_id: user.id).present?
+  end
 end
